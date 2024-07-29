01b5547ed703395e685a2bb7d14bbadd5c1b594a
abdullahBilalAH <abdullahbmah@gmail.com>
2024-07-29 10:12:45 +0300

    ///

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -37,4 +37,36 @@ class OrdersController extends Controller
 
   return view('orders.show', compact('order', 'items'));
  }
+ public function chart()
+ {
+  // Fetch the orders data
+  // Fetch the number of orders for each month
+  $orders = DB::table('orders')
+   ->select(DB::raw('COUNT(*) as count'), DB::raw('MONTH(created_at) as month'))
+   ->groupBy(DB::raw('MONTH(created_at)'))
+   ->pluck('count', 'month');
+
+  // Format data for Chart.js
+  $labels = [];
+  $data = [];
+
+  for ($i = 1; $i <= 12; $i++) {
+   $labels[] = Carbon::createFromDate(null, $i, 1)->format('F'); // Month name
+   $data[] = $orders->get($i, 0); // Order count or 0 if no orders
+  }
+  $itemsOrders = [];
+  $ordersData = Order::pluck('order');
+  foreach ($ordersData as $or) {
+   foreach ($or['items'] as $it)
+    array_push($itemsOrders, $it);
+  }
+
+  // حساب تكرار كل رقم
+  $counts = array_count_values($itemsOrders);
+
+  // ترتيب التكرارات تنازلياً
+  arsort($counts);
+
+  return view('orders.chart', compact('labels', 'data'));
+ }
 }
